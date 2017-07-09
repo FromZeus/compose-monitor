78f1a288ef02a41ea7c9442fb3b2629df3c8807e
Trishkin, Daniil <daniil.trishkin@t-systems.com>
2017-07-10 02:53:56 +0300

    Add new exec parameters
      * Add new params:
        - `--no-recreate`
        - `--no-deps`
        - `--affect-only-running`
      * Add flake8 exceptions

--- compose_monitor/main.py
+++ compose_monitor/main.py
@@ -14,6 +14,13 @@ parser.add_argument("-e", "--external-scheduler", dest="scheduler",
     action="store_true", help="Don't use built-in timer")
 parser.add_argument("-o", "--options", dest="options",
     help="Options for project")
+parser.add_argument("--no-recreate", dest="norecreate",
+    action="store_true", help="Don't recreate, only pull")
+parser.add_argument("--no-deps", dest="nodeps",
+    action="store_true", help="Don't restart services dependencies")
+parser.add_argument("--affect-only-running", dest="running",
+    action="store_true",
+    help="Restart only running services and don't touch stopped")
 parser.add_argument("-l", "--log", dest="log",
     help="Redirect logging to file")
 args = parser.parse_args()
@@ -31,10 +38,13 @@ else:
 def main():
     try:
         if args.options is None:
-            monitor = Monitor(args.path, dict(), args.log)
+            monitor = Monitor(args.path, dict(),
+                args.norecreate, args.nodeps, args.running, args.log)
         else:
             monitor = Monitor(args.path,
-                dict(zip(args.options.split()[0::2], args.options.split()[1::2])), args.log)
+                dict(zip(args.options.split()[0::2],
+                         args.options.split()[1::2])),
+                args.norecreate, args.nodeps, args.running, args.log)
         log.info("Monitor created successfully")
 
         log.info("Starting of monitor...")
--- compose_monitor/monitor.py
+++ compose_monitor/monitor.py
@@ -26,7 +26,8 @@ class Capturing(list):
 
 
 class Monitor(object):
-    def __init__(self, path, options, filelog=None):
+    def __init__(self, path, options,
+                 norecreate, nodeps, running, filelog=None):
         global log
 
         if "log" not in globals():
@@ -41,6 +42,9 @@ class Monitor(object):
 
         self.path = path
         self.options = options
+        self.norecreate = norecreate
+        self.nodeps = nodeps
+        self.running = running
         try:
             self.project = command.project_from_options(self.path,
                                                         self.options)
@@ -49,14 +53,45 @@ class Monitor(object):
                 format(traceback.format_exc()))
             raise SystemExit
 
+    def _pullup(self):
+        try:
+            self.project.pull()
+            log.info("Images pulled successfully")
+        except Exception:
+            log.error("Can't pull images")
+            raise
+
+        if self.norecreate:
+            return
+
+        if self.running:
+            services = [el.name for el in self._fully_running_services()]
+        else:
+            services = None
+
+        try:
+            self.project.up(services, start_deps=not self.nodeps)
+            log.info("Project updated successfully")
+        except Exception:
+            log.error("Can't update project")
+            raise
+
+    def _fully_running_services(self):
+        for service in self.project.get_services():
+            fully_running = []
+            # There is a presence of unhealthy behavior,
+            # `service.containers(stopped=True)`
+            # also may give running containers, so
+            if all(container.is_running for container in service.containers()):
+                fully_running.append(service)
+        return fully_running
+
     def run(self, timeout, scheduler):
         log.info("Monitor started successfully")
         while True:
             try:
                 with Capturing() as output:
-                    for service in self.project.services:
-                        service.pull()
-                    self.project.up()
+                    self._pullup()
             except Exception:
                 log.error("Service checking failed\n{}".
                     format(traceback.format_exc()))
